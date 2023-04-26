da06889a02cbf56e0bc02966f43951bea90918af
Richard <richard.m.sullivan2@gmail.com>
2023-04-26 10:18:10 -0500

    finished app and deployment to fly.io

--- app.py
+++ app.py
@@ -1,31 +1,26 @@
+import os
 import csv
 import json
-import os
-import requests
 import whoosh
-from  whoosh.highlight import HtmlFormatter
-from  whoosh.highlight import ContextFragmenter
+import requests
 
-from dotenv import load_dotenv, find_dotenv
-from flask import Flask, redirect, render_template, request, url_for, session
 from os import getenv
+from flask import Flask, redirect, render_template, request, url_for, session
+from dotenv import load_dotenv, find_dotenv
 from serpapi import GoogleSearch
-from whoosh.index import create_in
-from whoosh.fields import *
-from whoosh.index import open_dir
-from whoosh.qparser import QueryParser, OrGroup
+from whoosh.index import create_in, open_dir
 from whoosh.query import *
+from whoosh.fields import *
+from whoosh.qparser import QueryParser, OrGroup
+from whoosh.highlight import HtmlFormatter, ContextFragmenter
 
-
-ix = open_dir("index")
-searcher = ix.searcher()
-searcher.fragmenter = ContextFragmenter(maxchars=100, surround=20, charlimit=32768)
-searcher.formatter = HtmlFormatter(tagname='b',between='...<br>',classname='match',termclass='',maxclasses=1)
-local_results = None
-web_results = None
 
 def whoosh_query(query):
 # open the index, which stores all the values in the corpus
+    ix = open_dir("index")
+    searcher = ix.searcher()
+    searcher.fragmenter = ContextFragmenter(maxchars=100, surround=20, charlimit=32768)
+    searcher.formatter = HtmlFormatter(tagname='b',between='...<br>',classname='match',termclass='',maxclasses=1)
 
 # build a query parser, so that we can make queries
     or_group = OrGroup.factory(0.9)
@@ -43,58 +38,60 @@ load_dotenv(find_dotenv())
 SERP_KEY = getenv('SERP_KEY')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
 @app.route('/')
 def start():
+    session['web_results'] = None
+    session['local_query'] = None
     return redirect(url_for('google_search'))
 
 @app.route('/google',methods=['GET','POST'])
 def google_search():
-    global web_results
+    results=None
 
     # if the user prformed a query, parse the results and display
     if request.method == 'POST':
-        print('google')
         query = request.form['query']
+        
+        if query:
+            params = {
+              'engine': 'google',
+              'gl':'us',
+              'hl':'en',
+              'q': query,
+              'api_key': SERP_KEY
+            }
 
-        params = {
-          'engine': 'google',
-          'gl':'us',
-          'hl':en,
-          'q': query,
-          'api_key': SERP_KEY
-        }
+            search = GoogleSearch(params)
+            search_results = search.get_dict()
+            session['web_results'] = search_results['organic_results']
 
-        search = GoogleSearch(params)
-        search_results = search.get_dict()
-        web_results = search_results['organic_results']
+    if session['web_results']:
+        results = session['web_results']
 
-    return render_template('google.html', results=web_results)
+    return render_template('google.html', results=results)
 
 @app.route('/whoosh',methods=['GET','POST'])
 def whoosh_search():
-    
-    global local_results
-
+    result = None
     # if the user performed a query, parse the results and display
     if request.method == 'POST':
-        query = request.form['query']
+        session['local_query'] = request.form['query']
         # Use the Whoosh library to search for the query
         # Return the search results in an HTML template
-        local_results = whoosh_query(query)
 
-    return render_template('whoosh.html', results=local_results)
+    if session['local_query']:
+        result = whoosh_query(session['local_query'])
+
+    return render_template('whoosh.html', results=result)
 
 @app.route('/web-display/<num>')
 def web_display(num):
 
-    global local_results
-
-    return render_template('lyrics.html',result=local_results[int(num)])
+    return render_template('lyrics.html',result=whoosh_query(session['local_query'])[int(num)])
 
 @app.route('/local-display/<num>')
 def local_display(num):
 
-    global local_results
-
-    return render_template('lyrics.html',result=local_results[int(num)])
+    return render_template('lyrics.html',result=whoosh_query(session['local_query'])[int(num)])
